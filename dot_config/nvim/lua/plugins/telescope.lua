70171035a2c2c31d74afbf9134a36a9408e1860e
Felix Boenke <felixboenke@googlemail.com>
2026-06-16 23:08:23 +0200

    feat: setup telescope resume

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ telescope.setup({})
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume last Telescope search" })
 vim.keymap.set("n", "<leader>cd", function()
 	builtin.diagnostics({ bufnr = 0 })
 end, { desc = "List diagnostics in current buffer" })
@@ -13,7 +14,15 @@ vim.keymap.set("n", "<leader>cD", function()
 	builtin.diagnostics({})
 end, { desc = "List all diagnostics (global)" })
 
-vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find symbols in current buffer" })
-vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, { desc = "Find symbols in workspace" })
+vim.keymap.set("n", "<leader>fs", function()
+	builtin.lsp_document_symbols({
+		symbols = { "Function", "Method", "Class", "Constructor", "Interface", "Struct" },
+	})
+end, { desc = "Find symbols in current buffer" })
+vim.keymap.set("n", "<leader>fS", function()
+	builtin.lsp_workspace_symbols({
+		symbols = { "Function", "Method", "Class", "Constructor", "Interface", "Struct" },
+	})
+end, { desc = "Find symbols in workspace" })
 
 vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "Fuzzy switch buffer" })
